8c389ca2f9d5906f57e7a9b1076c00b1b13265c4
AnesuMurakata <murac96@gmail.com>
2025-04-30 15:28:41 +0200

    register client endpoint

--- employeeapp/views.py
+++ employeeapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from employeeapp.models import Client
 
 # Create your views here.
+class ClientAPIView(APIView):
+    def post(self, request):
+        try:
+            name = request.data["name"]
+            email = request.data["email"]
+
+            client_object = Client(name=name, email=email)
+
+            client_object.save()
+
+            return Response({"status": "success", "data": "Client registered successfully."}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            print('debug exception ', e)
+            return Response({"status": "error", "data": {"message": "Failed to register client.", "error": e}}, status=status.HTTP_400_BAD_REQUEST)
